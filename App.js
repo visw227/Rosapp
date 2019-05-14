c315d69f755479aa5791013225037ace85390c46
Viswa Chikkala <vchikkala@rosnet.com>
2019-05-14 15:13:52 -0500

    work around

--- App.js
+++ App.js
@@ -12,3 +12,8 @@ import App from './App/Rosnet'
 // import App from './Experiments/PasswordChange'
 
 export default App
+
+
+
+/// had an error related to importing 'fs' issue.. Don't really know where 'fs' is being imported
+// removed 'fs' from nodemodules/
