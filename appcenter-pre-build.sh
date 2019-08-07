a3116feb9045e4d019f77c96bf7e56a8d7e06ebb
djohnsonkc <djohnsonkc@gmail.com>
2019-08-07 13:33:28 -0500

    trying things in pre build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+echo -n 'Dywayne: Replacing ROSNET_APP_ENV in /App/Helpers/Config.js...'
 
-echo ">>> Replacing ROSNET_APP_ENV in Config.js..."
+echo -n 'CUSTOM_ROSNET_APP_ENV: $CUSTOM_ROSNET_APP_ENV'
 
 sed -i 's/ROSNET_APP_ENV/appcenter/g' /App/Helpers/Config.js
 
-echo ">>> REPLACED"
+echo 'appcenter-pre-build.sh DONE!'
 
