b21c8148e478ba6d8b667d8edd9f6f9fc77a1adf
djohnsonkc <djohnsonkc@gmail.com>
2019-08-07 14:41:10 -0500

    bash updated

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env bash
 
-echo -n 'Dywayne: Replacing ROSNET_APP_ENV in /App/Helpers/Config.js...'
+echo "Dywayne: Replacing the ROSNET_APP_ENV string value in /App/Helpers/Config.js..."
 
-if [ -z "$ROSNET_APP_ENV_VAR" ]
-then
-    echo "You need define the ROSNET_APP_ENV_VAR variable in App Center, with a value of appcenter"
-    exit
-fi
-
+# the full path to the file we are targeting
 CONFIG_FILE=$APPCENTER_SOURCE_DIRECTORY/App/Helpers/Config.js
 
-# sed -i 's/ROSNET_APP_ENV/appcenter/g' /App/Helpers/Config.js
+echo "Updating the env to 'appcenter' in $CONFIG_FILE"
+sed -i '' -e 's/ROSNET_APP_ENV/appcenter/g' $CONFIG_FILE
+
+echo "Updated contents:"
+cat $CONFIG_FILE
 
 
-if [ -e "$CONFIG_FILE" ]
-then
-    echo "Updating the env to $ROSNET_APP_ENV_VAR in /App/Helpers/Config.js"
-    sed -i '' -e 's/ROSNET_APP_ENV/'$ROSNET_APP_ENV_VAR'/g' $CONFIG_FILE
 
-    echo "Updated contents:"
-    cat $CONFIG_FILE
-
-    echo "appcenter-pre-build.sh finished"
-fi
