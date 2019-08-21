9bad8c863ec2ae62baa5c3909c9d7e1eab4ae22a
djohnsonkc <djohnsonkc@gmail.com>
2019-08-21 10:19:14 -0500

    on lauch resume session override fixes

--- App/Helpers/OnAppLaunchOrResume.js
+++ App/Helpers/OnAppLaunchOrResume.js
@@ -11,13 +11,14 @@ since they both can:
 
 Here is some psuedo code: 
 
+
 if NOT logged in (or first use)
 
     redirect to login screen
 
 if logged in
 
-    rehydrate the global state from local storage
+    if launch, rehydrate the global state from local storage
 
     if time to show biometric screen...
         show biometric screen
@@ -25,7 +26,7 @@ if logged in
                 update the global state
                     redirect to last screen or any forced actions (e.g. required password change)
 
-    if not time for biometric
+    else
         refresh the token
             update the global state
                 redirect to last screen or any forced actions
@@ -45,6 +46,7 @@ import NavigationService from './NavigationService';
 import brand from '../Styles/brand'
 
 export var OnAppLaunchOrResume = {
+
 
     GetAllKeys: function(callback) {
 
@@ -120,8 +122,11 @@ export var OnAppLaunchOrResume = {
 
                 console.log("userData, but token is null")
 
-                // userData exists, so must have logged in before, so rehydrate state
-                globalStateChange({ action: "launch", userData: data.userData } )
+                // no need to rehydrate the state if the app is already running
+                if(event === 'launch') {
+                    // userData exists, so must have logged in before, so rehydrate state
+                    globalStateChange({ action: "launch", userData: data.userData } )
+                }
 
                 NavigationService.navigate('LoginStack')
 
@@ -133,16 +138,19 @@ export var OnAppLaunchOrResume = {
 
                 console.log("userData & token...", data.userData)
                 
-                // share what was rehydrated, but they may change later in the logic...
-                globalStateChange({ action: "launch", userData: data.userData })
-                globalStateChange({ action: "launch", selectedClient: data.selectedClient  })
+                // no need to rehydrate the state if the app is already running
+                if(event === 'launch') {
+                    // share what was rehydrated, but they may change later in the logic...
+                    globalStateChange({ action: "launch", userData: data.userData })
+                    globalStateChange({ action: "launch", selectedClient: data.selectedClient  })
+                }
 
                 // IMPORTANT:
                 // Rehydrate the superUser object if available 
                 // THIS IS ESPECIALLY important if a superUser exits/closes/terminates the app entirely.
                 // Otherwise, when the superUser re-launches the app, they will be "really" logged in as an impersonated person
-                if(data.superUser) {
-                    //globalStateChange({ action: "launch", superUser: data.superUser } )
+                // no need to rehydrate the state if the app is already running
+                if(event === 'launch' && data.superUser) {
 
                     globalStateChange({ 
                         action: "session-override", 
@@ -169,8 +177,8 @@ export var OnAppLaunchOrResume = {
                 }
 
                 // if lastScreen was LockScreen, don't let the user force close the app to get around the lock screen
-                //if(!__DEV__ && (diff >= limit || (data.lastScreen && data.lastScreen === 'LockScreen')) ) {
-                if( diff >= limit || (data.lastScreen && data.lastScreen === 'LockScreen') ) {
+                if(!__DEV__ && (diff >= limit || (data.lastScreen && data.lastScreen === 'LockScreen')) ) {
+                //if( diff >= limit || (data.lastScreen && data.lastScreen === 'LockScreen') ) {
 
                     // if forced action, otherwise redirect to lastScreen after biometrics
                     if(data.userData.mustChangePassword ===  true) {
@@ -188,46 +196,52 @@ export var OnAppLaunchOrResume = {
                 }
                 else {
 
-                    // CANNOT use .RefreshToken because it may be a QA session override
-                    // and the impersonated userData.password is not really known and contains "***"
-                    // Instead, just verify the token AFTER the user sees the biometric screen
-                    let token = data.userData.token
-                    let client = data.selectedClient
-                    Authorization.VerifyToken(client, token, function(err, resp){
+                    if(event === 'activate') {
+                        return callback(data)
+                    }
+                    else if(event === 'launch') {
+                        // CANNOT use .RefreshToken because it may be a QA session override
+                        // and the impersonated userData.password is not really known and contains "***"
+                        // Instead, just verify the token AFTER the user sees the biometric screen
+                        let token = data.userData.token
+                        let client = data.selectedClient
+                        Authorization.VerifyToken(client, token, function(err, resp){
 
-                        if(err) {
+                            if(err) {
 
-                            // forced login
-                            NavigationService.navigate('LoginStack')
+                                // forced login
+                                NavigationService.navigate('LoginStack')
 
-                            callback(data)
+                                callback(data)
 
-                        }
-                        else {
-
-                            console.log("token refreshed")
-
-                            // if we are refreshing the token, we must reset all global state attributes back to defaults as well
-                            globalStateChange( { action: "launch", userData: resp.userData })
-                            // set/reset selectedClient
-                            globalStateChange( { action: "launch", selectedClient: resp.selectedClient  })
-
-
-                            // look for any forced actions that the user might still need to address
-                            if(data.userData.mustChangePassword ===  true) {
-                                // forced password change
-                                NavigationService.navigate('PasswordChangeRequiredStack');
                             }
                             else {
-                                // resume at last screen
-                                NavigationService.navigate(data.lastScreen);
-                            }
 
-                            return callback(data)
-                        
-                        } // end else
+                                console.log("token refreshed")
 
-                    }) // end Authorization.RefreshToken
+                                // if we are refreshing the token, we must reset all global state attributes back to defaults as well
+                                globalStateChange( { action: "launch", userData: resp.userData })
+                                // set/reset selectedClient
+                                globalStateChange( { action: "launch", selectedClient: resp.selectedClient  })
+
+
+                                // look for any forced actions that the user might still need to address
+                                if(data.userData.mustChangePassword ===  true) {
+                                    // forced password change
+                                    NavigationService.navigate('PasswordChangeRequiredStack');
+                                }
+                                else {
+                                    // resume at last screen
+                                    NavigationService.navigate(data.lastScreen);
+                                }
+
+                                return callback(data)
+                            
+                            } // end else
+
+                        }) // end Authorization.RefreshToken
+
+                    } // end if event = launch
 
                 } // end if( diff >= data.statusData.limit
 
